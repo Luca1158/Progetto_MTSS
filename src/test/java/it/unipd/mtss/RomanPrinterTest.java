b48cea38dbca513a040fc9aa349c01132e69c3c9
Riccardo Pozzato <riccardopozzato@MacBook-Pro-di-Riccardo.local>
2026-05-04 18:30:38 +0200

    Implementata conversione per i numeri fino a 10

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -40,4 +40,25 @@ public class RomanPrinterTest {
             RomanPrinter.print(6)
         );
     }
+
+    @Test
+       public void testPrintFirstTenNumbers() {
+           assertEquals(
+               " __      __  _____   _____ \n \\ \\    / / |_   _| |_   _|\n  \\ \\  / /    | |     | |  \n   \\ \\/ /     | |     | |  \n    \\  /     _| |_   _| |_ \n     \\/     |_____| |_____|\n", 
+               RomanPrinter.print(7)
+           );
+           assertEquals(
+               " __      __  _____   _____   _____ \n \\ \\    / / |_   _| |_   _| |_   _|\n  \\ \\  / /    | |     | |     | |  \n   \\ \\/ /     | |     | |     | |  \n    \\  /     _| |_   _| |_   _| |_ \n     \\/     |_____| |_____| |_____|\n", 
+               RomanPrinter.print(8)
+           );
+           assertEquals(
+               "  _____  __   __\n |_   _| \\ \\ / /\n   | |    \\ V / \n   | |     > <  \n  _| |_   / . \\ \n |_____| /_/ \\_\\\n", 
+               RomanPrinter.print(9)
+           );
+           assertEquals(
+               " __   __\n \\ \\ / /\n  \\ V / \n   > <  \n  / . \\ \n /_/ \\_\\\n", 
+               RomanPrinter.print(10)
+           );
+       }
+
 }
