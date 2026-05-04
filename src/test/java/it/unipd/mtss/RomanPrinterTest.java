4a9cdc996c29ef1127580797e419794ea925850e
Luca1158 <lucabrunetta1@gmail.com>
2026-05-04 17:48:10 +0200

    Conversione dei primi 6 numeri interi

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -24,4 +24,20 @@ public class RomanPrinterTest {
             RomanPrinter.print(3)
         );
     }
+
+    @Test
+    public void testPrintFirstSixNumbers() {
+        assertEquals(
+            "  _____  __      __\n |_   _| \\ \\    / /\n   | |    \\ \\  / / \n   | |     \\ \\/ /  \n  _| |_     \\  /   \n |_____|     \\/    \n", 
+            RomanPrinter.print(4)
+        );
+        assertEquals(
+            " __      __\n \\ \\    / /\n  \\ \\  / / \n   \\ \\/ /  \n    \\  /   \n     \\/    \n", 
+            RomanPrinter.print(5)
+        );
+        assertEquals(
+            " __      __  _____ \n \\ \\    / / |_   _|\n  \\ \\  / /    | |  \n   \\ \\/ /     | |  \n    \\  /     _| |_ \n     \\/     |_____|\n", 
+            RomanPrinter.print(6)
+        );
+    }
 }
