7cc25262d03bb829e13d4e4a90d0c09c69d28000
Luca1158 <lucabrunetta1@gmail.com>
2026-05-04 20:09:12 +0200

    Conversione dei primi 1000 numeri interi

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -12,8 +12,8 @@ public class IntegerToRoman {
             throw new IllegalArgumentException("Il numero deve essere compreso tra 1 e 1000");
         }
 
-        String[] romanLetters = {"D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        int[] values = {500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanLetters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
         StringBuilder romanNumber = new StringBuilder();
 
